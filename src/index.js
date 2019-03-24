7a1cc768351448c612fa5b83ffc3ca1a989ccc02
nataliamotuz <natalia.motuz@gmail.com>
2019-03-24 19:43:04 +0300

    solve lovetriangles

--- src/index.js
+++ src/index.js
@@ -3,5 +3,14 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+    let triangles = 0;
+    for (let i = 0; i < preferences.length; i++) {
+      let firstSpichonee = preferences[i] -1 ;
+      let secondSpichonee = preferences[firstSpichonee] -1;
+      let thirdSpichonee = preferences[secondSpichonee] -1;
+      if (thirdSpichonee === i && (firstSpichonee !== secondSpichonee)) {
+        triangles++;
+      }
+    }
+    return triangles / 3;
+  };
